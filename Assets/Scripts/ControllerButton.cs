205bc0b53a9b7fbaf857c1b106a4c89a5563a5a5
Banujan Atputharajah <banujan59@gmail.com>
2025-03-22 11:09:49 -0400

    Added line renderer

--- Assets/Scripts/ControllerButton.cs
+++ Assets/Scripts/ControllerButton.cs
@@ -5,10 +5,22 @@ using UnityEngine;
 public class ControllerButton : MonoBehaviour
 {
     public AudioSource source;
+    public LineRenderer linePrefab;
+    public Transform shootingPoint;
+    public float maxLineDistance = 5;
+    public float lineShowTimer = 0.3f;
+
     public AudioClip shootingAudioClip;
 
     public void FireProjectile()
     {
         source.PlayOneShot(shootingAudioClip);
+        LineRenderer line = Instantiate(linePrefab);
+        line.positionCount = 2;
+        line.SetPosition(0, shootingPoint.position);
+
+        Vector3 endPoint = shootingPoint.position + shootingPoint.forward * maxLineDistance;
+        line.SetPosition(1, endPoint);
+        Destroy(line.gameObject, lineShowTimer);
     }
 }
