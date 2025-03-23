96e6b0c2839f5f5d4ba1abf84fcda46150327860
moooooolly123 <serena1yu@icloud.com>
2025-03-23 09:10:05 -0400

    passage

---
+++ Assets/Scripts/PassageAudio.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PassageAudio : MonoBehaviour
+{
+    public LevelLoader levelLoader;
+
+    public AudioSource audioSource;
+    public AudioClip roomEntryClip;
+    public AudioClip wbcClip;
+
+    void Start()
+    {
+        PlayAudioAndSpawn();
+    }
+
+    void PlayAudioAndSpawn()
+    {
+        audioSource.PlayOneShot(roomEntryClip);
+        Invoke("PlayWBCClip", roomEntryClip.length); 
+    }
+
+    void PlayWBCClip()
+    {
+        audioSource.PlayOneShot(wbcClip);
+        Invoke("LoadNextScene", wbcClip.length); 
+    }
+
+    void LoadNextScene()
+    {
+        levelLoader.LoadNextScene();
+    }
+}
