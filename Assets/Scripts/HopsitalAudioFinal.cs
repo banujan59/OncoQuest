d47f03cffdadf3ca0f3010c86ebff7405c243110
moooooolly123 <serena1yu@icloud.com>
2025-03-23 09:48:33 -0400

    everything is up

---
+++ Assets/Scripts/HopsitalAudioFinal.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HopsitalAudioFinal : MonoBehaviour
+{
+    public LevelLoader levelLoader;
+    public AudioSource source;
+    public AudioClip congrats;
+
+
+    void Start()
+    {
+        source.PlayOneShot(congrats);
+        Invoke("OnTimerElapsed", 3.0f);
+    }
+}
