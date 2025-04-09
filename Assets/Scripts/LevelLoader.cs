791a881cfa21f44c15c18599430c5e77dc798d84
Banujan Atputharajah <banujan59@gmail.com>
2025-04-08 20:21:30 -0400

    Use coroutine to load next scene

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -6,7 +7,20 @@ public class LevelLoader : MonoBehaviour
     public void LoadNextScene()
     {
         Scene currentScene = SceneManager.GetActiveScene();
-        int currentSceneIdx = currentScene.buildIndex;
-        SceneManager.LoadScene(currentSceneIdx+1);
+        int targetSceneIdx = (currentScene.buildIndex + 1) % SceneManager.sceneCountInBuildSettings;
+        StartCoroutine(LoadSceneRoutine(targetSceneIdx));
+    }
+
+    private IEnumerator LoadSceneRoutine(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+        operation.allowSceneActivation = false;
+
+        while(operation.progress < 0.9f)
+        {
+            yield return null;
+        }
+
+        operation.allowSceneActivation = true;
     }
 }
